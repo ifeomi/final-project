8fcbf9ebd9599517ee5ceaefe20bef7af652c78b
Ife Omidiran <omidiran@college.harvard.edu>
2018-11-22 22:49:10 +0000

    Added users table and began updates to register

--- application.py
+++ application.py
@@ -41,7 +41,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///ClubPub.db")
+db = SQL("sqlite:///finance.db")
 
 
 @app.route("/")
